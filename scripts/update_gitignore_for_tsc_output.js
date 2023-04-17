a965e613bd7881bb613a0b72682c2ae7e10d61e4
Timothy Ng <timothy@leaselock.com>
2023-04-17 14:47:27 -0700

    remove prepare script and commit js output

--- scripts/update_gitignore_for_tsc_output.js
+++ scripts/update_gitignore_for_tsc_output.js
@@ -36,44 +36,44 @@ function main(cliCommand) {
   if (cliCommand === 'run') {
     console.log('Generating lib/.gitignore ...');
 
-    // Find all .ts files in lib/
-    const directoriesToProcess = [libDirectory];
-    const tsFiles = [];
-    while (directoriesToProcess.length > 0) {
-      const directory = directoriesToProcess.pop();
-      if (!fs.existsSync(directory)) {
-        throw new Error("Directory doesn't exist:", directory);
-      }
+    // // Find all .ts files in lib/
+    // const directoriesToProcess = [libDirectory];
+    // const tsFiles = [];
+    // while (directoriesToProcess.length > 0) {
+    //   const directory = directoriesToProcess.pop();
+    //   if (!fs.existsSync(directory)) {
+    //     throw new Error("Directory doesn't exist:", directory);
+    //   }
 
-      const files = fs.readdirSync(directory);
-      files.forEach((file) => {
-        const filename = path.join(directory, file);
-        const stat = fs.lstatSync(filename);
-        if (stat.isDirectory()) {
-          directoriesToProcess.push(filename);
-        } else if (filename.endsWith('.ts') && !filename.endsWith('.d.ts')) {
-          tsFiles.push(filename);
-          console.log('Found .ts file:', filename);
-        }
-      });
-    }
+    //   const files = fs.readdirSync(directory);
+    //   files.forEach((file) => {
+    //     const filename = path.join(directory, file);
+    //     const stat = fs.lstatSync(filename);
+    //     if (stat.isDirectory()) {
+    //       directoriesToProcess.push(filename);
+    //     } else if (filename.endsWith('.ts') && !filename.endsWith('.d.ts')) {
+    //       tsFiles.push(filename);
+    //       console.log('Found .ts file:', filename);
+    //     }
+    //   });
+    // }
 
-    // Get paths of .js files to ignore
-    const jsFilesToIgnore = tsFiles.map((filepath) => {
-      // Cuts off `${libDirectory}/`
-      const relativeTsPath = filepath.slice(libDirectory.length + 1);
-      // Swaps .ts for .js file ending
-      const relativeJsPath =
-        relativeTsPath.slice(0, relativeTsPath.length - 3) + '.js';
-      // Always use POSIX-style path separators - .gitignore requires it
-      return relativeJsPath.split(path.sep).join(path.posix.sep);
-    });
-    const jsFilesToIgnoreString = jsFilesToIgnore.join('\n');
-    const libGitignorePath = path.join(libDirectory, '.gitignore');
-    fs.writeFileSync(
-      libGitignorePath,
-      gitignoreHeader + jsFilesToIgnoreString + '\n'
-    );
+    // // Get paths of .js files to ignore
+    // const jsFilesToIgnore = tsFiles.map((filepath) => {
+    //   // Cuts off `${libDirectory}/`
+    //   const relativeTsPath = filepath.slice(libDirectory.length + 1);
+    //   // Swaps .ts for .js file ending
+    //   const relativeJsPath =
+    //     relativeTsPath.slice(0, relativeTsPath.length - 3) + '.js';
+    //   // Always use POSIX-style path separators - .gitignore requires it
+    //   return relativeJsPath.split(path.sep).join(path.posix.sep);
+    // });
+    // const jsFilesToIgnoreString = jsFilesToIgnore.join('\n');
+    // const libGitignorePath = path.join(libDirectory, '.gitignore');
+    // fs.writeFileSync(
+    //   libGitignorePath,
+    //   gitignoreHeader + jsFilesToIgnoreString + '\n'
+    // );
     console.log('DONE');
   } else if (['help', '--help', '-h', undefined].includes(cliCommand)) {
     console.log(helpText);
